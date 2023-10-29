fd1e17a5315aa0f4824f9ca684de53b1ea5b69f6
Mete Sayan <metesayan99@gmail.com>
2023-10-29 19:02:26 +0300

    feat: Swagger and configurations

---
+++ config/default.js
@@ -0,0 +1,9 @@
+module.exports = {
+    configName: 'defaultConfig',
+    serverConfig: {
+      port: 3200,
+      secondServerPort: 3201,
+      hsts: false,
+    },
+  };
+  
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,49 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
+import * as config from 'config';
+import * as compression from 'compression';
+import { json, urlencoded } from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+const port = (config.get('serverConfig.port') as number) || 3200;
+const hsts = config.get('serverConfig.hsts');
+declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(helmet({ hsts }));
+  app.use(compression());
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidUnknownValues: false,
+    }),
+  );
+  app.enableCors({ credentials: true, origin: '*' });
+
+  const apiPath = 'api';
+  app.setGlobalPrefix(apiPath);
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Nest-js E-commerce API')
+    .setDescription('E-commerce API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup(`${apiPath}/docs`, app, document);
+
+  await app.listen(port || 3200);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
+
 bootstrap();
